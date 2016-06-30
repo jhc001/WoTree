28e6f315a496fdeba5d937f25223542d0af2adee
jhc001 <964706001@qq.com>
2016-06-30 10:02:19 +0800

    删掉了一个没用的参数

--- js/wotree.js
+++ js/wotree.js
@@ -1,7 +1,7 @@
 (
   function(){
      var  WoTree = function(treedom,template){
-        return new WoTree.prototype.init(treedom,template);
+        return new WoTree.prototype.init(treedom);
     };
 
     WoTree.prototype = {
